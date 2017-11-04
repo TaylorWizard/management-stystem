e7a40447e5dae185a5657643ecba56420f1cf992
gu <376977792@qq.com>
2017-11-04 23:46:46 +0800

    login-module

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import SiderCustom from './components/SiderCustom'
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h1>App!!!!</h1>
+        <SiderCustom />
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import router from './routes/index'
+import {AppContainer} from 'react-hot-loader'
 import './index.css'
 
-ReactDOM.render(
-  <div>
-    <h1>management system</h1>
-  </div>,
-  document.getElementById('root')
-)
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
+render(router)
+if (module.hot) {
+  const orgError = console.error
+  console.error = (...args) => {
+
+  }
+  module.hot.accept('./routes', () => {
+    render(router)
+  })
+}
+
+
+
